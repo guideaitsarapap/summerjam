85090c7aebe165e28a3c6cdb54286995b6b31d8b
GuideAitsarapap <aitsarapap.work@gmail.com>
2026-04-17 20:17:57 +0700

    [Add]New scene for UI testing
    - add new scene for ui test
    - create script for healthbar in HealthManager

---
+++ Assets/Scripts/Health/HealthManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour
+{
+    public Image player1HealthBar;
+    public Image player2HealthBar;
+
+    public float player1MaxHealth = 100;
+    public float player2MaxHealth = 100;
+    public float player1CurrentHealth;
+    public float player2CurrentHealth;
+
+
+    public void TakeDamage(int playerNumber, int damageAmount)
+    {
+        if (playerNumber == 1)
+        {
+            player1CurrentHealth -= damageAmount;
+            Debug.Log("Player 1 Health: " + player1CurrentHealth);
+        }
+        else if (playerNumber == 2)
+        {
+            player2CurrentHealth -= damageAmount;
+            Debug.Log("Player 2 Health: " + player2CurrentHealth);
+        }
+    }
+
+    public void UpdateHealthBar()
+    {
+        player1HealthBar.fillAmount = player1CurrentHealth / player1MaxHealth;
+        player2HealthBar.fillAmount = player2CurrentHealth / player2MaxHealth;
+    }
+}
